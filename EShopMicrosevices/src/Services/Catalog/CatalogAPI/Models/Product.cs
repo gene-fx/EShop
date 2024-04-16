7da685f2c2002e72bde49795059486cb7704a8ad
gene-fx <a.rodrigues.jr@live.com>
2024-04-16 14:22:45 -0300

    add Models\Product

---
+++ EShopMicrosevices/src/Services/Catalog/CatalogAPI/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace CatalogAPI.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = default!;
+
+        public List<string> Category { get; set; } = new();
+
+        public string Description { get; set; }
+
+        public string ImageFile { get; set; } = default!;
+
+        public decimal Price { get; set; }
+    }
+}
