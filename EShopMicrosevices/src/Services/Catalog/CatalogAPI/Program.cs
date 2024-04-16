061c71b6c14b49f90b92e661fd074294fe8cd51b
gene-fx <a.rodrigues.jr@live.com>
2024-04-16 15:51:50 -0300

    Add Carter and MediatR services to the CatalogAPI container

--- EShopMicrosevices/src/Services/Catalog/CatalogAPI/Program.cs
+++ EShopMicrosevices/src/Services/Catalog/CatalogAPI/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(_ =>
+{
+    _.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapCarter();
 
 app.Run();
