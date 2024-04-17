e815d72d5842eab8d096b4126688b7a6012d6f88
gene-fx <a.rodrigues.jr@live.com>
2024-04-17 16:15:45 -0300

    add GetProductByCategory and change the GetProductByName endpoint to avoid error CatalogAPI

---
+++ EShopMicrosevices/src/Services/Catalog/CatalogAPI/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,35 @@
+﻿
+using CatalogAPI.Products.GetProductByName;
+using Mapster;
+
+namespace CatalogAPI.Products.GetProductByCategory
+{
+    //public record GetProductByCategoryRequest();
+
+    public record GetProductByCategoryResponse(IEnumerable<Product> Products);
+
+    public class GetProductByCategoryEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/{category}", async (ISender sender, string category) =>
+            {
+                var result = await sender.Send(new GetProductByCategoryQuery(category));
+
+                GetProductByCategoryResponse response = result.Adapt<GetProductByCategoryResponse>();
+
+                if(response is null || response.Products.IsEmpty())
+                {
+                    throw new ProductNotFoundException();
+                }
+
+                return Results.Ok(response);
+            })
+            .WithName("GetProductByCategory")
+            .Produces<GetProductByNameResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Product By Category")
+            .WithDescription("Get Product By Category");
+        }
+    }
+}
---
+++ EShopMicrosevices/src/Services/Catalog/CatalogAPI/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,22 @@
+﻿using Marten.Linq.QueryHandlers;
+using System.Linq;
+
+namespace CatalogAPI.Products.GetProductByCategory
+{
+    public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+
+    internal class GetProductByCategoryQueryHandler(IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger)
+        : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation($"GetProductByNameQueryHandler.Handle called with {query}");
+
+            var response = await session.Query<Product>().Where(_ => _.Category.Contains(query.Category)).ToListAsync();
+
+            return new GetProductByCategoryResult(response);
+        }
+    }
+}
