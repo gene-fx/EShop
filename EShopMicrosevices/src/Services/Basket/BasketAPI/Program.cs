5d2ac3eeecd5572584dc975fb0dc68e1f53b1986
gene-fx <a.rodrigues.jr@live.com>
2024-05-08 16:10:59 -0300

    add HealthChecks to BasketAPI and orchestrate it with docker

--- EShopMicrosevices/src/Services/Basket/BasketAPI/Program.cs
+++ EShopMicrosevices/src/Services/Basket/BasketAPI/Program.cs
@@ -1,5 +1,7 @@
 using BasketAPI.Data.Repository;
 using BasketAPI.Data.Repository.IRepository;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +17,7 @@ builder.Services.AddMediatR(config =>
 
 builder.Services.AddMarten(opts =>
 {
-    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Connection(builder.Configuration.GetConnectionString("Postgres")!);
     opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
 }).UseLightweightSessions();
 
@@ -29,10 +31,19 @@ builder.Services.AddStackExchangeRedisCache(opts =>
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Postgres")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapCarter();
 app.UseExceptionHandler(options => { });
+app.UseHealthChecks("/health", 
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
         
 app.Run();  
