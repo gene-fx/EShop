061afa20401740f0419826b9aee85c4777c8c4d7
gene-fx <a.rodrigues.jr@live.com>
2025-02-12 14:43:32 -0300

    Add DeleteOrder to OrderingApplication layer

---
+++ EShopMicrosevices/src/Services/Ordering/OrderingApplication/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace OrderingApplication.Orders.Commands.DeleteOrder;
+
+public record DeleteOrderCommand(Guid Id) : ICommand<DeleteOrderResult>;
+
+public record DeleteOrderResult(bool IsSuccess);
+
+public class DeleOrderCommandValidator : AbstractValidator<DeleteOrderCommand>
+{
+    public DeleOrderCommandValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty().WithMessage("OrderId must be informed");
+    }
+}
---
+++ EShopMicrosevices/src/Services/Ordering/OrderingApplication/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace OrderingApplication.Orders.Commands.DeleteOrder;
+public class DeleteOrderCommandHandler(IApplicationDbContext dbContext)
+    : ICommandHandler<DeleteOrderCommand, DeleteOrderResult>
+{
+    public async Task<DeleteOrderResult> Handle(DeleteOrderCommand command, CancellationToken cancellationToken)
+    {
+        var order = await dbContext.Orders.FindAsync([OrderId.Of(command.Id)], cancellationToken);
+
+        if (order is null) throw new OrderNotFoundException(command.Id);
+
+        dbContext.Orders.Remove(order);
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new DeleteOrderResult(true);
+    }
+
+}
