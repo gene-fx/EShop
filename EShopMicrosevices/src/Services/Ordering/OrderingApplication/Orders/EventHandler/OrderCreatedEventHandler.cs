305212d82be0cd58f3e7d6c8fad0b04cf4f22690
gene-fx <a.rodrigues.jr@live.com>
2025-02-13 16:37:04 -0300

    Add OrderCreatedEventHandler and OrderUpdatedEventHandler to OrderingApplicaiton layer

---
+++ EShopMicrosevices/src/Services/Ordering/OrderingApplication/Orders/EventHandler/OrderCreatedEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using OrderingDomain.Evenvts;
+
+namespace OrderingApplication.Orders.EventHandler;
+
+public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger)
+    : INotificationHandler<OrderCreatedEvent>
+{
+    public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Domain Event handled: {^DomainEvent}", notification.GetType().Name);
+        return Task.CompletedTask;
+    }
+}
---
+++ EShopMicrosevices/src/Services/Ordering/OrderingApplication/Orders/EventHandler/OrderUpdatedEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using OrderingDomain.Evenvts;
+
+namespace OrderingApplication.Orders.EventHandler;
+public class OrderUpdatedEventHandler(ILogger<OrderUpdatedEventHandler> logger)
+    : INotificationHandler<OrderCreatedEvent>
+{
+    public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Domain Event handled: {^DomainEvent}", notification.GetType().Name);
+        return Task.CompletedTask;
+    }
+}
