fa79ccb26fd5470bdd8106c395fec8e146329700
gene-fx <a.rodrigues.jr@live.com>
2025-02-14 18:27:17 -0300

    Create Extensions folder under OrderingApplication. Move ProjectToOrederDto from GetOrdersByNameHandler to a newly created class OrderExtensions

---
+++ EShopMicrosevices/src/Services/Ordering/OrderingApplication/Extensions/OrderExtions.cs
@@ -0,0 +1,36 @@
+﻿namespace OrderingApplication.Extensions;
+public static class OrderExtions
+{
+    public static IReadOnlyCollection<OrderDto> ProjectToOrderDto(this IReadOnlyCollection<OrderDto> orders)
+    {
+        return orders.Select(order => new OrderDto(
+                Id: order.Id,
+                CustomerID: order.CustomerID,
+                OrderName: order.OrderName,
+                ShippingAddress: new AddressDto(
+                    order.ShippingAddress.FirstName,
+                    order.ShippingAddress.LastName,
+                    order.ShippingAddress.EmailAddress,
+                    order.ShippingAddress.AddressLine,
+                    order.ShippingAddress.Country,
+                    order.ShippingAddress.State,
+                    order.ShippingAddress.ZipCode),
+                BillingAddress: new AddressDto(
+                    order.BillingAddress.FirstName,
+                    order.BillingAddress.LastName,
+                    order.BillingAddress.EmailAddress,
+                    order.BillingAddress.AddressLine,
+                    order.BillingAddress.Country,
+                    order.BillingAddress.State,
+                    order.BillingAddress.ZipCode),
+                Payment: new PaymentDto(
+                    order.Payment.CardName,
+                    order.Payment.CardNumber,
+                    order.Payment.Expiration,
+                    order.Payment.Cvv,
+                    order.Payment.PaymentMethod),
+                Status: order.Status,
+                OrderItems: order.OrderItems.Select(oi => new OrderItemDto(oi.OrderId, oi.ProductId, oi.Quantity, oi.Price)).ToList().AsReadOnly()
+            )).ToList().AsReadOnly();
+    }
+}
--- EShopMicrosevices/src/Services/Ordering/OrderingApplication/Orders/Queries/GetOrdersByName/GetOrdersByNameHandler.cs
+++ EShopMicrosevices/src/Services/Ordering/OrderingApplication/Orders/Queries/GetOrdersByName/GetOrdersByNameHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace OrderingApplication.Orders.Queries.GetOrdersByName;
+﻿using OrderingApplication.Extensions;
+
+namespace OrderingApplication.Orders.Queries.GetOrdersByName;
 public class GetOrdersByNameHandler(IApplicationDbContext dbContext)
     : IQueryHandler<GetOrdersByNameQuery, GetOrdersByNameResult>
 {
@@ -11,49 +13,47 @@ public class GetOrdersByNameHandler(IApplicationDbContext dbContext)
             .ToListAsync()
             .ContinueWith(result => result.Result as IReadOnlyList<OrderDto>);
 
-        var ordersDto = ProjectToOrderDto(orders!);
-
-        return new GetOrdersByNameResult(ordersDto);
+        return new GetOrdersByNameResult(orders!.ProjectToOrderDto());
     }
 
-    private IReadOnlyList<OrderDto> ProjectToOrderDto(IReadOnlyList<OrderDto> orders)
-    {
-        var result = new List<OrderDto>();
+    //private IReadOnlyList<OrderDto> ProjectToOrderDto(IReadOnlyList<OrderDto> orders)
+    //{
+    //    var result = new List<OrderDto>();
 
-        foreach (var order in orders)
-        {
-            var orderDto = new OrderDto(
-                Id: order.Id,
-                CustomerID: order.CustomerID,
-                OrderName: order.OrderName,
-                ShippingAddress: new AddressDto(
-                    order.ShippingAddress.FirstName,
-                    order.ShippingAddress.LastName,
-                    order.ShippingAddress.EmailAddress,
-                    order.ShippingAddress.AddressLine,
-                    order.ShippingAddress.Country,
-                    order.ShippingAddress.State,
-                    order.ShippingAddress.ZipCode),
-                BillingAddress: new AddressDto(
-                    order.BillingAddress.FirstName,
-                    order.BillingAddress.LastName,
-                    order.BillingAddress.EmailAddress,
-                    order.BillingAddress.AddressLine,
-                    order.BillingAddress.Country,
-                    order.BillingAddress.State,
-                    order.BillingAddress.ZipCode),
-                Payment: new PaymentDto(
-                    order.Payment.CardName,
-                    order.Payment.CardNumber,
-                    order.Payment.Expiration,
-                    order.Payment.Cvv,
-                    order.Payment.PaymentMethod),
-                Status: order.Status,
-                OrderItems: order.OrderItems.Select(oi => new OrderItemDto(oi.OrderId, oi.ProductId, oi.Quantity, oi.Price)).ToList().AsReadOnly());
+    //    foreach (var order in orders)
+    //    {
+    //        var orderDto = new OrderDto(
+    //            Id: order.Id,
+    //            CustomerID: order.CustomerID,
+    //            OrderName: order.OrderName,
+    //            ShippingAddress: new AddressDto(
+    //                order.ShippingAddress.FirstName,
+    //                order.ShippingAddress.LastName,
+    //                order.ShippingAddress.EmailAddress,
+    //                order.ShippingAddress.AddressLine,
+    //                order.ShippingAddress.Country,
+    //                order.ShippingAddress.State,
+    //                order.ShippingAddress.ZipCode),
+    //            BillingAddress: new AddressDto(
+    //                order.BillingAddress.FirstName,
+    //                order.BillingAddress.LastName,
+    //                order.BillingAddress.EmailAddress,
+    //                order.BillingAddress.AddressLine,
+    //                order.BillingAddress.Country,
+    //                order.BillingAddress.State,
+    //                order.BillingAddress.ZipCode),
+    //            Payment: new PaymentDto(
+    //                order.Payment.CardName,
+    //                order.Payment.CardNumber,
+    //                order.Payment.Expiration,
+    //                order.Payment.Cvv,
+    //                order.Payment.PaymentMethod),
+    //            Status: order.Status,
+    //            OrderItems: order.OrderItems.Select(oi => new OrderItemDto(oi.OrderId, oi.ProductId, oi.Quantity, oi.Price)).ToList().AsReadOnly());
 
-            result.Add(order);
-        }
+    //        result.Add(order);
+    //    }
 
-        return result.AsReadOnly();
-    }
+    //    return result.AsReadOnly();
+    //}
 }
