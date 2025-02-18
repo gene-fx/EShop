4cd1cb5b72ccc8f1739e9c5b18288f1002ca33b4
gene-fx <a.rodrigues.jr@live.com>
2025-02-17 22:37:43 -0300

    Add DeleteOrder endpoint to OrderingApplication layer.

--- EShopMicrosevices/src/Services/Ordering/OrderingAPI/Endpoints/DeleteOrder.cs
+++ EShopMicrosevices/src/Services/Ordering/OrderingAPI/Endpoints/DeleteOrder.cs
@@ -1,5 +1,26 @@
-﻿namespace OrderingAPI.Endpoints;
+﻿using OrderingApplication.Orders.Commands.DeleteOrder;
 
-public class DeleteOrder
+namespace OrderingAPI.Endpoints;
+
+//public record DeleteOrderRequest(Guid Id);
+
+public record DeleteOrderResponse(bool IsSuccess);
+
+public class DeleteOrder : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("orders/{id}", (Guid id, ISender sender) =>
+        {
+            var result = sender.Send(new DeleteOrderCommand(id));
+
+            return Results.Ok(result.Adapt<DeleteOrderResponse>());
+        })
+        .WithName("DeleteOrder")
+        .Accepts<Guid>("Accepts the Order ID")
+        .Produces<DeleteOrderResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("DeleteOrder");
+    }
 }
