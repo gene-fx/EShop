ed8c88d34d2b183fc168ebb98db5260dc65b8304
gene-fx <a.rodrigues.jr@live.com>
2025-02-17 23:16:08 -0300

    Add GetOrders enpoint to the OrderingAPI layer

--- EShopMicrosevices/src/Services/Ordering/OrderingAPI/Endpoints/GetOrders.cs
+++ EShopMicrosevices/src/Services/Ordering/OrderingAPI/Endpoints/GetOrders.cs
@@ -1,5 +1,25 @@
-﻿namespace OrderingAPI.Endpoints;
+﻿using BuildingBlocks.Pgination;
+using OrderingApplication.Orders.Queries.GetOrders;
 
-public class GetOrders
+namespace OrderingAPI.Endpoints;
+
+public record GetOrdersResponse(PaginatedResult<OrderDto> Orders);
+
+public class GetOrders : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders", async ([AsParameters] PaginationRequest request, ISender sender) =>
+        {
+            var result = sender.Send(request.Adapt<GetOrdersQuery>());
+
+            return Results.Ok(result.Adapt<GetOrdersResponse>());
+        })
+        .WithName("GetOrders")
+        .Accepts<PaginationRequest>("Accepst the pagination variables as a parameter")
+        .Produces<GetOrdersResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Get Orders");
+    }
 }
