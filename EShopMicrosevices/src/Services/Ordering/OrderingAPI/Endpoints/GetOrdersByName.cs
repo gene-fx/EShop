8b7309cfc8861dc5c0c3a569b4ff1d2a5448e4f7
gene-fx <a.rodrigues.jr@live.com>
2025-02-17 22:22:40 -0300

    Add GetOrderByName endpoit to OrderingAPI layer

--- EShopMicrosevices/src/Services/Ordering/OrderingAPI/Endpoints/GetOrdersByName.cs
+++ EShopMicrosevices/src/Services/Ordering/OrderingAPI/Endpoints/GetOrdersByName.cs
@@ -1,5 +1,26 @@
-﻿namespace OrderingAPI.Endpoints;
+﻿using OrderingApplication.Orders.Queries.GetOrdersByName;
 
-public class GetOrdersByName
+namespace OrderingAPI.Endpoints;
+
+//public record GetOrdersByMameRequest(string Name);
+
+public record GetOrdersByMameResponse(IReadOnlyCollection<OrderDto> Orders);
+
+public class GetOrdersByName : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("orders/{name}", async (string name, ISender sender) =>
+        {
+            var result = await sender.Send(new GetOrdersByNameQuery(name));
+
+            return Results.Ok(result.Adapt<GetOrdersByMameResponse>());
+        })
+        .WithName("GetOredersByName")
+        .Accepts<string>("Accepts the Order Name")
+        .Produces<GetOrdersByMameResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("GetOrderByName");
+    }
 }
