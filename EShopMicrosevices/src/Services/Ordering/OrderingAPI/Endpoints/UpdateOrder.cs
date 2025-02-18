c1cea04f42f445736206aad26ebd66f06b421e8e
gene-fx <a.rodrigues.jr@live.com>
2025-02-17 22:53:27 -0300

    Add UpdateOrder endpoint to the OrderingAPI layer

--- EShopMicrosevices/src/Services/Ordering/OrderingAPI/Endpoints/UpdateOrder.cs
+++ EShopMicrosevices/src/Services/Ordering/OrderingAPI/Endpoints/UpdateOrder.cs
@@ -1,5 +1,25 @@
-﻿namespace OrderingAPI.Endpoints;
+﻿using OrderingApplication.Orders.Commands.UpdateOrder;
 
-public class UpdateOrder
+namespace OrderingAPI.Endpoints;
+
+public record UpdateOrderRequest(OrderDto Order);
+
+public record UpdateOrderResponse(bool IsSuccess);
+
+public class UpdateOrder : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/oders", async (UpdateOrderRequest request, ISender sender) =>
+        {
+            var result = await sender.Send(request.Adapt<UpdateOrderCommand>());
+
+            return Results.Ok(result.Adapt<UpdateOrderResponse>());
+        })
+        .WithName("UpdateOrder")
+        .Accepts<UpdateOrderRequest>("Accepts the Order model to update")
+        .Produces<UpdateOrderResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("UpdateOrder");
+    }
 }
