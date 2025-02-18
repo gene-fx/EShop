6f18c849bd368b22cb06b963d432bceebce84092
gene-fx <a.rodrigues.jr@live.com>
2025-02-17 23:05:14 -0300

    Add GetOrdersByCustomer endpoint to OrdertingAPI layer

--- EShopMicrosevices/src/Services/Ordering/OrderingAPI/Endpoints/GetOrdersByCustomer.cs
+++ EShopMicrosevices/src/Services/Ordering/OrderingAPI/Endpoints/GetOrdersByCustomer.cs
@@ -1,5 +1,26 @@
-﻿namespace OrderingAPI.Endpoints;
+﻿using OrderingApplication.Orders.Queries.GetOrdersByCustomer;
 
-public class GetOrdersByCustomer
+
+namespace OrderingAPI.Endpoints;
+
+//public record GetOrdersByCustomerNameResquest(Guid Customer);
+
+public record GetOrdersByCustomerResponse(IReadOnlyCollection<OrderDto> Orders);
+
+public class GetOrdersByCustomer : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders/customer/{customerId}", async (Guid customerId, ISender sender) =>
+        {
+            var result = await sender.Send(customerId.Adapt<GetOrdersByCustomerQuery>());
+
+            return Results.Ok(result.Adapt<GetOrdersByCustomerResponse>());
+        })
+        .WithName("GetOrdersByCustomer")
+        .Accepts<Guid>("Accepts the Customer ID")
+        .Produces<GetOrdersByCustomerResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Orders By Customer ID");
+    }
 }
