5ec113987aa127f16499682b820a9c1423e6a25a
gene-fx <a.rodrigues.jr@live.com>
2025-02-25 11:19:18 -0300

    Add HealthChecks for SQL database in OrderingAPI layer

--- EShopMicrosevices/src/Services/Ordering/OrderingAPI/DependencyInjection.cs
+++ EShopMicrosevices/src/Services/Ordering/OrderingAPI/DependencyInjection.cs
@@ -1,16 +1,19 @@
 ﻿using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 namespace OrderingAPI;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddCarter();
 
         services.AddExceptionHandler<CustomExceptionHandler>();
 
-        services.AddHealthChecks();
+        services.AddHealthChecks()
+            .AddSqlServer(configuration.GetConnectionString("Database")!);
 
         return services;
     }
@@ -21,7 +24,11 @@ public static class DependencyInjection
 
         webApplication.UseExceptionHandler(options => {/*emapty*/ });
 
-        webApplication.UseHealthChecks("/health");
+        webApplication.UseHealthChecks("/health",
+            new HealthCheckOptions
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
 
         return webApplication;
     }
