b2dba980a3b0b3263ace90561b579796e4a8da4b
gene-fx <a.rodrigues.jr@live.com>
2025-02-25 15:46:27 -0300

    Add MassTransit folder and Extentions class to it in BuildingBlocks.Messaging

---
+++ EShopMicrosevices/src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extentions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BuildingBlocks.Messaging.MassTransit;
+
+public static class Extentions
+{
+    public static IServiceCollection AddMessageBroker(this IServiceCollection services, Assembly? assembly = null)
+    {
+        //Implement RabbitMQ MassTransit configuration
+        return services;
+    }
+}
