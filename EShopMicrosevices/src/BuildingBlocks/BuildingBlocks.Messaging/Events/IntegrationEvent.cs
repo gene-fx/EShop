a918ac0b3331545dd7246721ff03967c460195c3
gene-fx <a.rodrigues.jr@live.com>
2025-02-25 14:54:29 -0300

    Add BuildingBlocks.Messaging class library. Add Integration event class and BasketCheckoutEvent class

---
+++ EShopMicrosevices/src/BuildingBlocks/BuildingBlocks.Messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace BuildingBlocks.Messaging.Events;
+
+public record IntegrationEvent
+{
+    public Guid Id => Guid.NewGuid();
+
+    public DateTime OccurredOn => DateTime.UtcNow;
+
+    public string EventType => GetType().AssemblyQualifiedName!;
+}
