2ca5e0c5ead6a3a5b63571f8e6d0bd761f3df184
gene-fx <a.rodrigues.jr@live.com>
2025-02-25 16:03:49 -0300

    Add Extentions class to BuildingBlocks.Messaging in order to register the MassTransit service

--- EShopMicrosevices/src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extentions.cs
+++ EShopMicrosevices/src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extentions.cs
@@ -1,13 +1,32 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace BuildingBlocks.Messaging.MassTransit;
 
 public static class Extentions
 {
-    public static IServiceCollection AddMessageBroker(this IServiceCollection services, Assembly? assembly = null)
+    public static IServiceCollection AddMessageBroker(this IServiceCollection services,
+        IConfiguration configuration, Assembly? assembly = null)
     {
-        //Implement RabbitMQ MassTransit configuration
+        services.AddMassTransit(config =>
+        {
+            config.SetKebabCaseEndpointNameFormatter();
+
+            if (assembly != null) config.AddConsumers(assembly);
+
+            config.UsingRabbitMq((context, configurator) =>
+            {
+                configurator.Host(new Uri(configuration["MessageBroker:Host"]!), host =>
+                {
+                    host.Username(configuration["MessageBroker:UserName"]!);
+                    host.Password(configuration["MessageBroker:Password"]!);
+                });
+                configurator.ConfigureEndpoints(context);
+            });
+        });
+
         return services;
     }
 }
