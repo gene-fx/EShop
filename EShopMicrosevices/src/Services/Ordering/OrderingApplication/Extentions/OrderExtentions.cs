52f82b9caa70c079804a3992ee4a65b7a8f2fc2c
gene-fx <a.rodrigues.jr@live.com>
2025-03-13 11:09:08 -0300

    Add ToOrderDto extention to OrderingApplication and configure OrderCreatedEventHandler

--- EShopMicrosevices/src/Services/Ordering/OrderingApplication/Extentions/OrderExtentions.cs
+++ EShopMicrosevices/src/Services/Ordering/OrderingApplication/Extentions/OrderExtentions.cs
@@ -24,7 +24,7 @@ public static class OrderExtentions
                     order.BillingAddress.State,
                     order.BillingAddress.ZipCode),
                 Payment: new PaymentDto(
-                    order.Payment.CardName,
+                    order.Payment.CardName!,
                     order.Payment.CardNumber,
                     order.Payment.Expiration,
                     order.Payment.CVV,
@@ -33,5 +33,39 @@ public static class OrderExtentions
                 OrderItems: order.OrderItems.Select(oi =>
                     new OrderItemDto(oi.OrderId.Value, oi.ProductId.Value, oi.Quantity, oi.Price)).ToList().AsReadOnly()
             )).ToList().AsReadOnly();
+    }
+
+    public static OrderDto ToOrderDto(this Order order)
+    {
+        return new OrderDto(
+            OrderId: order.Id.Value,
+            CustomerId: order.CustomerId.Value,
+            OrderName: order.OrderName.Value,
+            ShippingAddress: new AddressDto(
+                order.ShippingAddress.FirstName,
+                order.ShippingAddress.LastName,
+                order.ShippingAddress.EmailAddress,
+                order.ShippingAddress.AddressLine,
+                order.ShippingAddress.Country,
+                order.ShippingAddress.State,
+                order.ShippingAddress.ZipCode),
+            BillingAddress: new AddressDto(
+                order.BillingAddress.FirstName,
+                order.BillingAddress.LastName,
+                order.BillingAddress.EmailAddress,
+                order.BillingAddress.AddressLine,
+                order.BillingAddress.Country,
+                order.BillingAddress.State,
+                order.BillingAddress.ZipCode),
+            Payment: new PaymentDto(
+                order.Payment.CardName!,
+                order.Payment.CardNumber,
+                order.Payment.Expiration,
+                order.Payment.CVV,
+                order.Payment.PaymentMethod),
+            Status: order.Status,
+            OrderItems: order.OrderItems.Select(oi =>
+                new OrderItemDto(oi.OrderId.Value, oi.ProductId.Value, oi.Quantity, oi.Price)).ToList().AsReadOnly()
+        );
     }
 }
--- EShopMicrosevices/src/Services/Ordering/OrderingApplication/Orders/EventHandler/Domain/OrderCreatedEventHandler.cs
+++ EShopMicrosevices/src/Services/Ordering/OrderingApplication/Orders/EventHandler/Domain/OrderCreatedEventHandler.cs
@@ -1,11 +1,18 @@
-﻿namespace OrderingApplication.Orders.EventHandler.Domain;
+﻿using MassTransit;
 
-public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger)
+namespace OrderingApplication.Orders.EventHandler.Domain;
+
+public class OrderCreatedEventHandler(
+    IPublishEndpoint publishEndpoint,
+    ILogger<OrderCreatedEventHandler> logger)
     : INotificationHandler<OrderCreatedEvent>
 {
-    public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+    public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
     {
-        logger.LogInformation("Domain Event handled: {DomainEvent}", notification.GetType().Name);
-        return Task.CompletedTask;
+        logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
+
+        var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
+
+        await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
     }
 }
