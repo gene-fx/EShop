cc3ad7d68ee1f827ef26367754930be0c06375a2
gene-fx <a.rodrigues.jr@live.com>
2025-11-01 13:03:43 -0300

    add Yarp package to YarpApiGateway and add it to the container in Program.cs

--- EShopMicrosevices/src/ApiGateways/YarpApiGateway/Program.cs
+++ EShopMicrosevices/src/ApiGateways/YarpApiGateway/Program.cs
@@ -3,5 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 //Add services to the container.
 var app = builder.Build();
 
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 //Configure the HTTP request pipeline.
+app.MapReverseProxy();
+
 app.Run();
