3e6fa074308ea0e7da0b3817a4efa7387da86f44
gene-fx <a.rodrigues.jr@live.com>
2025-11-03 23:45:37 -0300

    Configures the YarpGateway to the docker compose.

--- EShopMicrosevices/src/ApiGateways/YarpApiGateway/Program.cs
+++ EShopMicrosevices/src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,12 +1,40 @@
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container.
-var app = builder.Build();
-
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("fixed", config =>
+    {
+        config.PermitLimit = 5;
+        config.Window = TimeSpan.FromSeconds(10);
+        config.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        config.QueueLimit = 0;
+    });
+
+    options.RejectionStatusCode = 429;
+
+
+    options.OnRejected = async (context, token) =>
+    {
+        var requestingIp = context.HttpContext.Connection.RemoteIpAddress?.ToString() ?? "unkown";
+        context.HttpContext.Response.StatusCode = 429;
+        context.HttpContext.Response.ContentType = "application/json";
+        context.HttpContext.Response.Headers.RetryAfter = "10";
+        await context.HttpContext.Response.WriteAsync($"Too many requests from ip {requestingIp}. Please try again later.", token);
+    };
+});
+
+var app = builder.Build();
 //Configure the HTTP request pipeline.
+
+app.UseRateLimiter();
+
 app.MapReverseProxy();
 
 app.Run();
