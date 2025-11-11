cffffe1dfbe4b0a5f47b9a7f6c2f968d42498f5a
gene-fx <a.rodrigues.jr@live.com>
2025-11-11 20:59:12 -0300

    Adds IOrderingService to WebApp

---
+++ EShopMicrosevices/src/WebApps/ShoppingWeb/Services/IOrderingService.cs
@@ -0,0 +1,16 @@
+﻿using Refit;
+using ShoppingWeb.Models.Ordering;
+
+namespace ShoppingWeb.Services;
+
+public interface IOrderingService
+{
+    [Get("/ordering-service/orders?pageIndex={pageIndex}&pageSize={pageSize}")]
+    Task<GetOrdersResponse> GetOrders(int? pageIndex = 1, int? pageSize = 10, CancellationToken cancellationToken = default);
+
+    [Get("/ordering-service/orders/user/{name}")]
+    Task<GetOrdersByNameResponse> GetOPrdersByName(string name, CancellationToken cancellationToken = default);
+
+    [Get("/ordering-service/orders/customer/{customerId}")]
+    Task<GetOrdersByCustomerResponse> GetOrdersByCustomer(string customerId, CancellationToken cancellationToken = default);
+}
